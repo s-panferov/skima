cfd2af2a808513489c70ea36b507798256e44117
Stanislav Panferov <stanislav@panferov.dev>
2023-05-06 14:54:52 -0700

    Better rsx example

--- examples/rsx.rs
+++ examples/rsx.rs
@@ -1,10 +1,22 @@
 #![feature(type_alias_impl_trait)]
 
+use bumpalo::Bump;
 pub use skima::web::prelude::*;
+use skima::web::ssr::StaticHtml;
+use skima::web::HtmlBackend;
 pub use skima_rsx::html;
 
+fn _button<B: HtmlBackend>() -> impl Markup<B> {
+	return html!(<button>"Buttom"</button>);
+}
+
 fn main() {
-	// html!(<div class="10">
-	// 	"Console log"
-	// </div>);
+	let markup = html!(<div class="10">
+		"Console log"
+	</div>);
+
+	let bump = Bump::new();
+	let backend = StaticHtml::new(&bump);
+
+	println!("{}", skima::web::ssr::render(backend, markup));
 }
